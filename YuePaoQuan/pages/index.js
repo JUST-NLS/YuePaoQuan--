9ea0e32a4e24ea7d180965292f9582601f4109dc
1043863315 <38759303+1043863315@users.noreply.github.com>
2018-07-09 17:53:35 +0800

    Add files via upload

---
+++ YuePaoQuan/pages/index.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    pageNames: [
+      {
+        id: 'animation',
+        name: '动画',
+      }, {
+        id: 'run',
+        name: '跑步',
+      }, {
+        id: 'chat',
+        name: '聊天',
+      }, {
+        id: 'logs',
+        name: '登陆记录',
+      }
+    ]
+  },
+});
